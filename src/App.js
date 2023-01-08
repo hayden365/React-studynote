180b30cd945d1589df0860c108861b84dc4fa391
Hyeji Kong <109419531+hayden365@users.noreply.github.com>
2023-01-08 23:03:12 +0900

    React state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+import ExpenseForm from './components/NewExpense/ExpenseForm';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+const App = () => {
 	const expenses = [
 		{
 			id: 'e1',
@@ -23,15 +25,17 @@ function App() {
 		},
 	];
 
+	const addExpenseHandler = expense => {
+		console.log('In App.js');
+		console.log(expense);
+	};
+
 	return (
 		<div>
-			<h2>Let's get started!</h2>
-			<ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-			<ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-			<ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-			<ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+			<NewExpense onAddExpense={addExpenseHandler} />
+			<Expenses items={expenses} />
 		</div>
 	);
-}
+};
 
 export default App;
