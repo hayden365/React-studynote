a8affa41b3e750a3d97263b4e670bd0408e7e52a
hayden365 <ysmh100@gmail.com>
2023-02-05 16:10:29 +0900

    complete

--- 5. 음식주문 앱/src/store/CartProvider.js
+++ 5. 음식주문 앱/src/store/CartProvider.js
@@ -9,8 +9,6 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === "ADD") {
-		const updatedItems = state.items.concat(action.item);
-
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount;
 
@@ -18,19 +16,38 @@ const cartReducer = (state, action) => {
 			item => item.id === action.item.id,
 		);
 		const existingCartItem = state.items[existingCartItemIndex];
-		let updatedItem;
 		let updatedItems;
 
 		if (existingCartItem) {
-			updatedItem = {
+			const updatedItem = {
 				...existingCartItem,
 				amount: existingCartItem.amount + action.item.amount,
 			};
-			updatedItem = [...state.items];
+			updatedItems = [...state.items];
 			updatedItems[existingCartItemIndex] = updatedItem;
 		} else {
-			updatedItem = { ...action.item };
 			updatedItems = state.items.concat(action.item);
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	if (action.type === "REMOVE") {
+		const existingCartItemIndex = state.items.findIndex(
+			item => item.id === action.id,
+		);
+
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter(item => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
 		}
 
 		return {
