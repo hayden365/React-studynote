9b879c9250fedb9a48fac0871a76485fe1cb556d
hayden365 <ysmh100@gmail.com>
2023-05-16 08:20:52 +0900

    react-hook-form usage

--- recoil/src/ToDoList.tsx
+++ recoil/src/ToDoList.tsx
@@ -30,11 +30,53 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList() {
-	const { register, watch } = useForm();
+	const { register, handleSubmit } = useForm();
+	const onValid = (data: any) => {
+		console.log(data);
+	};
+
 	return (
 		<div>
-			<form>
-				<input {...register("toDo")} placeholder="Write a to do" />
+			<form
+				style={{ display: "flex", flexDirection: "column" }}
+				onSubmit={handleSubmit(onValid)}
+			>
+				<input
+					{...register("Email", {
+						required: true,
+						pattern: {
+							value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+							message: "Only naver.com emails allowed",
+						},
+					})}
+					placeholder="Email"
+				/>
+				<input
+					{...register("firstName", { required: true })}
+					placeholder="FirstName"
+				/>
+				<input
+					{...register("lastName", { required: true })}
+					placeholder="LastName"
+				/>
+				<input
+					{...register("username", { required: true, minLength: 10 })}
+					placeholder="Username"
+				/>
+				<input
+					{...register("password", { required: true })}
+					placeholder="Password"
+				/>
+				<input
+					{...register("password1", {
+						required: "Pssword is required",
+						minLength: {
+							value: 5,
+							message: "Your password is too short.",
+						},
+					})}
+					placeholder="Password1"
+				/>
 				<button>Add</button>
 			</form>
 		</div>
